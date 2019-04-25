a7d4d6a4ef8695974aa074cb7a013ea69caadba8
hd-jung <dossa328@gmail.com>
2019-04-25 18:05:59 +0900

    initial commit

---
+++ vendor/bundle/bin/kramdown.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Ruby23-x64\bin\ruby.exe" "D:/hd-jung.github.io/vendor/bundle/bin/kramdown" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Ruby23-x64\bin\ruby.exe" "%~dpn0" %*
